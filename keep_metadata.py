04969aba7720355715f34fac3b392d546565872b
Grunnpi <pierre.grunnagel@gmail.com>
2020-04-07 14:45:42 +0200

    initial version - ffmpeg call not working

---
+++ keep_metadata.py
@@ -0,0 +1,41 @@
+import logging
+import sys
+import argparse
+import subprocess
+
+
+parser=argparse.ArgumentParser()
+
+# Ecole Directe cred
+parser.add_argument('--src', help='source', type=str)
+parser.add_argument('--dst', help='destination', type=str)
+parser.add_argument('--loglevel', help='log level', type=str, default="info")
+
+args=parser.parse_args()
+
+LEVELS = {'debug': logging.DEBUG,
+          'info': logging.INFO,
+          'warning': logging.WARNING,
+          'error': logging.ERROR,
+          'critical': logging.CRITICAL}
+
+FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+LOG_FILENAME = 'D:\\Documents\[Mes Images]\\[A trier]\\Handbrake_video_convertie\\keep_metadata.log'
+level_name = str(args.loglevel)
+level = LEVELS.get(level_name, logging.NOTSET)
+logging.basicConfig(filename=LOG_FILENAME,level=level,format=FORMAT)
+
+logging.info('Number of arguments: %s',len(sys.argv))
+logging.info('Argument List: %s',str(sys.argv))
+logging.info("source = %s",str(args.src))
+logging.info("destination = %s",str(args.dst))
+
+
+ffmgeg_exe = "d:\\devlog\\ffmpeg\\bin\\ffmpeg.exe"
+metadata_file = "D:\\metadata.txt"
+
+extract_metadata = subprocess.run([ffmgeg_exe, '--i "{0}"'.format(str(args.src)), "-c copy -map_metadata 0 -map_metadata:s:v 0:s:v -map_metadata:s:a 0:s:a -f ffmetadata", metadata_file])
+logging.info("extract return [%s]",extract_metadata)
+
+import_metadata = subprocess.run([ffmgeg_exe, '--i "{0}"'.format(str(args.dst)), '-i {0}'.format(metadata_file), "-map_metadata 1 -codec copy", str(args.dst) + ".v2"])
+logging.info("import return [%s]",import_metadata)
